e9b219cbd36908374a45c4ab67e512907dc47a40
Replit user <>
2023-08-03 08:24:11 +0000

    Initialize

--- src/commands.ts
+++ src/commands.ts
@@ -1,9 +1,31 @@
-import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
+import {
+  ApplicationCommandOptionType,
+  RESTPostAPIApplicationCommandsJSONBody,
+} from "discord-api-types/v10";
 
 const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
   {
     name: "ping",
     description: "Respond with Pong",
+  },
+  {
+    name: "check",
+    description: "Check OpenAI Keys",
+    options: [
+      {
+        type: ApplicationCommandOptionType.String,
+        name: "key",
+        description: "An OpenAI' Key",
+        required: true,
+        min_length: 40,
+        max_length: 60,
+      },
+      {
+        type: ApplicationCommandOptionType.Boolean,
+        name: "ephemeral",
+        description: "Ephemeral message",
+      },
+    ],
   },
 ];
 
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,23 @@ import {
 } from "discord-api-types/v10";
 import { getLogger } from "./logger";
 import { isChatInputApplicationCommandInteraction } from "discord-api-types/utils/v10";
+import {
+  APIApplicationCommandInteractionDataOption,
+  APIApplicationCommandInteractionDataStringOption,
+  APIApplicationCommandInteractionDataBooleanOption,
+  ApplicationCommandOptionType,
+} from "discord-api-types/v10";
+
+const isString = (
+  option: APIApplicationCommandInteractionDataOption,
+): option is APIApplicationCommandInteractionDataStringOption => {
+  return option.type === ApplicationCommandOptionType.String;
+};
+const isBoolean = (
+  option: APIApplicationCommandInteractionDataOption,
+): option is APIApplicationCommandInteractionDataBooleanOption => {
+  return option.type === ApplicationCommandOptionType.Boolean;
+};
 
 const app = new Hono();
 
@@ -40,6 +57,110 @@ app.post("/interactions", async (c) => {
             data: {
               content: "Pong!",
               flags: MessageFlags.Ephemeral,
+            },
+          });
+        }
+        case "check": {
+          logger.debug("handling check interaction");
+          const api = interaction.data.options?.filter(isString)[0];
+          const ephemeral = interaction.data.options?.filter(isBoolean)[0].value
+            ? MessageFlags.Ephemeral
+            : 0;
+          const [data, emptyData] = await Promise.all([
+            fetch("https://api.openai.com/v1/models", {
+              headers: {
+                Authorization: `Bearer ${api?.value}`,
+              },
+            }),
+            fetch("https://api.openai.com/v1/chat/completions", {
+              headers: {
+                Authorization: `Bearer ${api?.value}`,
+                "Content-Type": "application/json",
+              },
+              method: "POST",
+              body: JSON.stringify({ model: "gpt-3.5-turbo" }),
+            }),
+          ]);
+          const jsonData = await data.json();
+          const emptyJsonData = await emptyData.json();
+          logger.debug(jsonData);
+          logger.debug(emptyJsonData);
+          if (jsonData.error) {
+            if (jsonData.error.code === "invalid_api_key") {
+              return c.json({
+                type: InteractionResponseType.ChannelMessageWithSource,
+                data: {
+                  embeds: [
+                    {
+                      color: 0xff0000,
+                      description: "Incorrect API Key provided - Maybe revoked",
+                    },
+                  ],
+                  flags: ephemeral,
+                },
+              });
+            } else {
+              return c.json({
+                type: InteractionResponseType.ChannelMessageWithSource,
+                data: {
+                  embeds: [
+                    {
+                      color: 0xff0000,
+                      description:
+                        "Unexpected Error - " + jsonData.error.message,
+                    },
+                  ],
+                  flags: ephemeral,
+                },
+              });
+            }
+          }
+          if (
+            emptyJsonData.error &&
+            emptyJsonData.error.code === "insufficient_quota"
+          ) {
+            return c.json({
+              type: InteractionResponseType.ChannelMessageWithSource,
+              data: {
+                embeds: [
+                  {
+                    color: 0xff0000,
+                    description: "Exceeded current quota - No money",
+                  },
+                ],
+                flags: ephemeral,
+              },
+            });
+          }
+          if (data.status === 429) {
+            return c.json({
+              type: InteractionResponseType.ChannelMessageWithSource,
+              data: {
+                content: "429: Please try again.",
+                flags: ephemeral,
+              },
+            });
+          }
+          const models = jsonData.data
+            .filter((x: { object: "model" }) => x.object === "model")
+            .map((x: { id: string }) => x.id);
+          return c.json({
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+              embeds: [
+                {
+                  color: 0x00ff00,
+                  description: "Available",
+                  fields: [
+                    {
+                      name: "GPT-4",
+                      value: `${models.includes("gpt-4") ? "🟢" : "🔴"}`,
+                    },
+                    { name: "Usable models", value: models.join(",") },
+                  ],
+                },
+              ],
+              flags: ephemeral,
             },
           });
         }
